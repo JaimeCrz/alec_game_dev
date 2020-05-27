8e816843c37e33b0c0174a34d0d8ad44d7bdcd87
Jaime <jaime.cruz.hostalot@gmail.com>
2020-05-27 16:18:52 +0200

    [Added] react-ga (Google Analytics)

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          blablabla
         </p>
       </header>
     </div>
