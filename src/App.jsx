3cef8caf45f264c1b0f450ff3c0861600d572581
Jaime <jaime.cruz.hostalot@gmail.com>
2020-05-27 17:47:55 +0200

    [Legacy] setting up files to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactGA from 'react-ga';
 import $ from 'jquery';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
@@ -14,13 +13,8 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      foo: 'bar',
       resumeData: {}
     };
-
-    ReactGA.initialize('UA-110570651-1');
-    ReactGA.pageview(window.location.pathname);
-
   }
 
   getResumeData(){
