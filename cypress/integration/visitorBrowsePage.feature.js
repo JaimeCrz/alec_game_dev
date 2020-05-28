122e600f62041c50047d2bb215fd298094a0100d
Jaime <jaime.cruz.hostalot@gmail.com>
2020-05-28 15:22:09 +0200

    [Cypress] Figured issue with cypress testing. start cypress 1 in the script.

--- cypress/integration/visitorBrowsePage.feature.js
+++ cypress/integration/visitorBrowsePage.feature.js
@@ -1,5 +1,13 @@
+
 describe("Visitor can see page information.", () => {
   beforeEach(() => {
     cy.visit("/");
+  });
+
+  it('Visitor sees ', () => {
+    cy.get('#home').within(() => {
+      cy.get('h1').should('contain', "I'm Alexander Pettersson.") 
+      cy.get('textarea').type('is a developer') 
+    })
   });
 });
